e705fe9aab71a2b1e450fb953f038ff05e97720b
victor <vicopem@gmail.com>
2024-04-02 15:26:08 -0400

    Fix: Toastify

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import store from "@/store";
 import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }) {
   return (
